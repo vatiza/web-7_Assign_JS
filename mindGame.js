1c9e88d11b7db0c4a6787e1b816a97696d56a861
vatiza <rakibur.mac@gmail.com>
2023-02-10 17:16:26 +0600

    add mind Game

---
+++ mindGame.js
@@ -0,0 +1,11 @@
+function mindGame(number) {
+  const multip3=number*3;
+  const add10=multip3+10;
+    const divis2 = add10 / 2;
+    let result=divis2 -5;
+    return result;
+    
+
+}
+const positiveNumber= mindGame(33);
+console.log(positiveNumber)
